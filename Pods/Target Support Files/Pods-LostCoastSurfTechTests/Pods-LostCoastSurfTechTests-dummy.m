39070a95b2015b8eebb9c5757fc51ec7a2d827fe
Devin Nicholson <devin@Devins-MacBook-Pro.local>
2018-01-19 16:31:57 -0800

    created podfile

---
+++ Pods/Target Support Files/Pods-LostCoastSurfTechTests/Pods-LostCoastSurfTechTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LostCoastSurfTechTests : NSObject
+@end
+@implementation PodsDummy_Pods_LostCoastSurfTechTests
+@end
---
+++ Pods/Target Support Files/Pods-LostCoastSurfTechUITests/Pods-LostCoastSurfTechUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LostCoastSurfTechUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LostCoastSurfTechUITests
+@end
